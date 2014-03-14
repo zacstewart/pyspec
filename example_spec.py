6aadee753fc4f231ab1d02e5ba2a6561fbce1d02
Zac Stewart <zgstewart@gmail.com>
2014-03-14 16:12:46 -0400

    fixup! Treat non-AssertionError as failures and other Errors as errors

--- example_spec.py
+++ example_spec.py
@@ -19,25 +19,34 @@ if __name__ == '__main__':
         def is_buzz(self, n):
             return n % 5 == 1
 
-    with description(BrokenFizzBuzz):
+    with description('stuff'):
+        with description(BrokenFizzBuzz):
 
-        fizzbuzz = BrokenFizzBuzz()
-        some_integer = 7
+            fizzbuzz = BrokenFizzBuzz()
+            some_integer = 7
 
-        with description('.convert'):
+            with description('.convert'):
 
-            with context(15):
-                with specification('returns fizzbuzz'):
-                    expect(fizzbuzz.convert(15)).to(eq('fizzbuzz'))
+                with context(15):
+                    with specification('returns fizzbuzz'):
+                        expect(fizzbuzz.convert(15)).to(eq('fizzbuzz'))
 
-            with context(3):
-                with specification('returns fizz'):
-                    expect(fizzbuzz.convert(3)).to(eq('fizz'))
+                with context(3):
+                    with specification('returns fizz'):
+                        expect(fizzbuzz.convert(3)).to(eq('fizz'))
 
-            with context(5):
-                with specification('returns buzz'):
-                    expect(fizzbuzz.convert(5)).to(eq('buzz'))
+                with context(5):
+                    with specification('returns buzz'):
+                        expect(fizzbuzz.convert(5)).to(eq('buzz'))
 
-            with context('some other integer'):
-                with specification('returns the integer'):
-                    expect(fizzbuzz.convert(some_integer)).to(eq(some_integer))
+                with context('some other integer'):
+                    with specification('returns the integer'):
+                        expect(fizzbuzz.convert(some_integer)).to(eq(some_integer))
+
+            with description('.floop'):
+                with specification('does not exist'):
+                    expect(fizzbuzz.floop()).to(eq('frigg'))
+
+        with description('normal assertions'):
+            with specification('are treated as failures and not errors'):
+                assert False, "False wasn't True!"
