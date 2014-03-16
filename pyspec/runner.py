b518ac41bbbe8130f99c40e72a4c43fd2572c375
Zac Stewart <zgstewart@gmail.com>
2014-03-16 16:04:02 -0400

    Support running entire dirs of spec files

--- pyspec/runner.py
+++ pyspec/runner.py
@@ -1,19 +1,27 @@
 from argparse import ArgumentParser
-from glob import glob
+import fnmatch
+import os
 import pyspec
 
 
 def main(argv=None, prog=None, **kwargs):
     """The console runner for PySpec"""
 
-    parser = ArgumentParser()
-    parser.add_argument('specs_path',
-                        type=str,
-                        help='The path to a spec or a directory of specs')
+    parser = ArgumentParser(argv)
+    parser.add_argument('spec_path',
+                        help='The path to a spec or a directory of specs',
+                        nargs='?',
+                        default='spec')
     options = parser.parse_args()
 
-    for path in glob(options.specs_path):
-        execfile(path)
+    if os.path.isdir(options.spec_path):
+        for root, dirnames, filenames in os.walk(options.spec_path):
+            for filename in fnmatch.filter(filenames, '*_spec.py'):
+                execfile(os.path.join(root, filename))
+    elif os.path.isfile(options.spec_path):
+        execfile(options.spec_path)
+    else:
+        print("Spec not found: {0}".format(options.spec_path))
 
     pyspec.suite.report_results()
 
